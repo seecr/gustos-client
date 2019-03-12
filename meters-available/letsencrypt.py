3a79b28c84d36bd498f6aef50753212d17c29a29
Johan Jonkers <johan@seecr.nl>
2019-03-12 12:49:34 +0100

    JJ: meter for letsencrypt renewals

---
+++ meters-available/letsencrypt.py
@@ -0,0 +1,28 @@
+## begin license ##
+#
+# "Gustos" is a monitoring tool by Seecr. This client side code for connecting with Gustos server.
+#
+# Copyright (C) 2019 Seecr (Seek You Too B.V.) https://seecr.nl
+#
+# This file is part of "Gustos-Client"
+#
+# "Gustos-Client" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Gustos-Client" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Gustos-Client"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from gustos.client.letsencryptrenewals import LetsEncryptRenewals
+
+def meter():
+    return dict(meter=LetsEncryptRenewals(), interval=60*60)
